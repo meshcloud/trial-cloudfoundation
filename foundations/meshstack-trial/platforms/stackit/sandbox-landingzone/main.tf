bbf5eec01cb0e6e395618ea769a6793667277fa1
Jelle den Burger <jdburger@meshcloud.io>
2026-07-03 16:50:56 +0200

    chore: update new reference-architecture ref
    This was moved meanwhile (it was not broken but always nicer to update to the latest state ^^)

--- foundations/meshstack-trial/platforms/stackit/sandbox-landingzone/main.tf
+++ foundations/meshstack-trial/platforms/stackit/sandbox-landingzone/main.tf
@@ -21,7 +21,7 @@ data "meshstack_workspace" "owner" {
 }
 
 module "stackit_sandbox_landingzone" {
-  source = "github.com/meshcloud/meshstack-hub//modules/stackit/sandbox-landingzone?ref=f7ccadc426610a2d06b37e3bb916f7bbfdfab1cb"
+  source = "github.com/meshcloud/meshstack-hub//reference-architectures/stackit-sandbox-landingzone?ref=e53005a3fab495fa433486469d9f792942b63be9"
 
   meshstack = {
     owning_workspace_identifier = data.meshstack_workspace.owner.metadata.name
