c9d670a08fba7e9184cae4beaa1c11b6337a5614
Henry Dettmer <hdettmer@meshcloud.io>
2026-07-13 15:36:05 +0200

    chore: update stackit

--- foundations/meshstack-trial/platforms/stackit/sandbox-landingzone/main.tf
+++ foundations/meshstack-trial/platforms/stackit/sandbox-landingzone/main.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     meshstack = {
       source  = "meshcloud/meshstack"
-      version = ">= 0.22.0"
+      version = ">= 0.23.1"
     }
   }
 }
@@ -21,34 +21,45 @@ data "meshstack_workspace" "owner" {
 }
 
 module "stackit_sandbox_landingzone" {
-  source = "github.com/meshcloud/meshstack-hub//reference-architectures/stackit-sandbox-landingzone?ref=e53005a3fab495fa433486469d9f792942b63be9"
+  source = "github.com/meshcloud/meshstack-hub//reference-architectures/stackit-sandbox-landingzone?ref=main"
 
   meshstack = {
     owning_workspace_identifier = data.meshstack_workspace.owner.metadata.name
   }
 
   hub = {
-    git_ref   = "feature/stackit-project-custom-roles"
+    git_ref   = "main"
     bbd_draft = true
   }
 }
 
 
-resource "meshstack_building_block_v2" "stackit_sandbox_landingzone" {
+resource "meshstack_building_block" "stackit_sandbox_landingzone" {
   spec = {
-    # Alternatively, use version_latest_release to target only released versions
     building_block_definition_version_ref = module.stackit_sandbox_landingzone.building_block_definition.version_ref
 
     display_name = "STACKIT Sandbox Landingzone"
     target_ref   = data.meshstack_workspace.owner.ref
 
     inputs = {
-      platform_identifier         = { value_string = "stackit-sandbox" }
-      tags                        = { value_code = jsonencode({ building_block = {}, landingzone = {} }) }
-      stackit_org                 = { value_string = "05d7eb3f-f875-4bcd-ad0d-a07d62787f21" }
-      stackit_owner_email         = { value_string = "stackit@meshcloud.io" }
-      stackit_service_account_key = { value_code_sensitive = var.stackit_service_account_key }
-      use_global_location         = { value_bool = true }
+      platform_identifier = { value = jsonencode("stackit-sandbox") }
+      tags = { value = jsonencode(jsonencode({
+        building_block = { Company = ["stackit-university"] },
+        landingzone    = { Company = ["stackit-university"] }
+        }))
+      }
+      stackit_org         = { value = jsonencode("05d7eb3f-f875-4bcd-ad0d-a07d62787f21") }
+      stackit_owner_email = { value = jsonencode("stackit@meshcloud.io") }
+      stackit_service_account_key = { sensitive = {
+        secret_value   = var.stackit_service_account_key
+        secret_version = nonsensitive(sha256(var.stackit_service_account_key))
+      } }
+      use_global_location = { value = jsonencode(true) }
     }
   }
+}
+
+moved {
+  from = meshstack_building_block_v2.stackit_sandbox_landingzone
+  to   = meshstack_building_block.stackit_sandbox_landingzone
 }
